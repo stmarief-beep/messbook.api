b4fe0036190949a52b78fd1f15775336da11cb73
stmarief-beep <stmarief@gmail.com>
2026-02-11 11:59:49 +0300

    Update server.js
    fix catch-all path.

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Catch-all route to serve the frontend for any non-API requests
-app.get('(.*)', (req, res) => {
+app.get('/:path*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
