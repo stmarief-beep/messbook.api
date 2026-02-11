23b21f8b01f4bf6827714b4dbaf5acf77398a5a5
stmarief-beep <stmarief@gmail.com>
2026-02-11 14:17:03 +0300

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -2,13 +2,16 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME || 'messbook', 
-    process.env.DB_USER || 'root', 
-    process.env.DB_PASS || '', 
+    process.env.DB_NAME || 'messbook',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASS || '',
     {
         host: process.env.DB_HOST || 'localhost',
         dialect: 'mysql',
-        logging: false
+        logging: false,
+        define: {
+            freezeTableName: true
+        }
     }
 );
 
